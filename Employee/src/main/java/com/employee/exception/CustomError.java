7d99ad4152bdbd268f4dd4935d3752df951854ed
maithreyan <91974819+maithreyan-kr@users.noreply.github.com>
2022-08-10 13:42:13 +0530

    Add files via upload

---
+++ Employee/src/main/java/com/employee/exception/CustomError.java
@@ -0,0 +1,18 @@
+package com.employee.exception;
+
+import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import lombok.Getter;
+import lombok.Setter;
+@Getter
+@Setter
+public class CustomError {
+	
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd hh:mm:ss")
+	private LocalDateTime timestamp;
+    private int status;
+    private String error;
+
+}
